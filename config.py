f94540271d62243950594278fe73c63afc421368
Devan Anjelito <anjelitodevan@gmail.com>
2024-01-04 18:39:27 +0700

    create config

---
+++ config.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+month_num = 12
+year = 2023
+
+exclude_cpt = True
+
+path_att = Path(
+    "../data - coco class session and attendance/data/12 dec 2023/data attendance dec 2023.xlsx"
+)
+path_member_count = Path(
+    "../data - experience management report/input/Member Population.xlsx"
+)
