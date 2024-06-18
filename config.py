6a87a95f833838367323423f5c9b42a244d282d0
Devan Anjelito <anjelitodevan@gmail.com>
2024-06-18 09:39:51 +0700

    run for may 2024

--- config.py
+++ config.py
@@ -1,16 +1,19 @@
 from pathlib import Path
 
-month_num = 1
+month_num = 5
 year = 2024
 
 exclude_cpt = True
 
 path_att = Path(
-    "/home/anj/Documents/wse-local/2. Experience/data - coco class session and attendance/output/2024/2024-01/data-attendance-2024-01.xlsx"
+    "/home/anj/Documents/wse-local/2. Experience/data - coco class session and attendance/output/2024/2024-05/data-attendance-2024-05.xlsx"
 )
 path_sess = Path(
-    "/home/anj/Documents/wse-local/2. Experience/data - coco class session and attendance/output/2024/2024-01/data-session-2024-01.xlsx"
+    "/home/anj/Documents/wse-local/2. Experience/data - coco class session and attendance/output/2024/2024-05/data-session-2024-05.xlsx"
 )
 path_member_count = Path(
     "/home/anj/Documents/wse-local/2. Experience/dependencies/Member Population (Sync with Local).xlsx"
+)
+path_member = Path(
+    "/home/anj/Documents/wse-local/2. Experience/data - coco member/output/2024-05/coco_member.xlsx"
 )
